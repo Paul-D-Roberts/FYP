d12d6744a9b59081e9a54071b16d438d0b49a35d
Paul Roberts <paulroberts130@hotmail.com>
2015-03-25 23:56:56 +0000

    Deleted duplicate graph. Added second subgraph.

--- relations.py
+++ relations.py
@@ -2,13 +2,15 @@
 # of every element being related to itself, turning
 # all graphs into a relation on itself.
 
-R = [[1, 2, 3], {'A': [1, 2, 'A'],
+R = [{1, 2, 3}, {'A': [1, 2, 'A'],
                  'B': [2, 3, 'B'],
                  1: [1],
                  2: [2],
                  3: [3]}]
 
-R2 = [[1, 2, 3], {'A': [1, 2, 'A'], 'B': [2, 3, 'B'], 1: [1], 2: [2], 3: [3]}]
+subR = [{3}, {3: [3]}]
 
-subR2 = [[3], {3: [3]}]
+subR2 = [{2, 3}, {'B': [2, 3, 'B'],
+                  2: [2], 3: [3]}]
+
 
